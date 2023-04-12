d7d0b58f0dca6fb966b39d51d983caefb0c062f4
frontendoniyorbek <tursunovdoniyorbek077@gmail.com>
2023-04-12 13:31:31 +0500

    add navbar components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import {Main, Login, Register} from './components'
+import {Main, Login, Register, Navbar} from './components'
 
 function App() {
   return (
     <div className="container">
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Main/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
       </Routes>
-      <h1>Salom</h1>
     </div>
   );
 }
